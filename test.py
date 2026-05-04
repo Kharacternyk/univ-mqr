bd30e2e522a1295b51a6b53aaad5087925dcf8be
Nazar Vinnichuk <nazar@vinnich.uk>
2026-05-04 20:37:28 +0300

    Create a new class.

---
+++ test.py
@@ -0,0 +1,79 @@
+from pathlib import Path
+from random import Random
+from subprocess import run
+
+import torchaudio
+from torch.nn.functional import pad
+from torchaudio.datasets import LIBRITTS
+from tqdm import tqdm
+
+from key import Key
+from watermark import Watermark
+
+key = Key(
+    channel_count=64,
+    kernel_size=5,
+    confidence_factor=1.8,
+    seed=0,
+    source_count=2,
+    target_count=3,
+)
+watermark = Watermark(
+    checkpoint="checkpoints/208000/model.pt",
+    key=key,
+    sample_rate=16_000,
+    shift=6,
+)
+
+dataset = LIBRITTS("./data/", "test-clean")
+subset = [dataset[i] for i in Random(0).sample(range(len(dataset)), 5)]
+
+for audio, sample_rate, *_, sample_id in tqdm(subset):
+    sample_dir = Path("test") / sample_id
+    sample_dir.mkdir(parents=True, exist_ok=True)
+
+    original_score, applied, reconstructed = watermark.check(
+        audio=audio, sample_rate=sample_rate, apply=True, reconstruct=True
+    )
+
+    assert applied is not None
+    assert reconstructed is not None
+
+    print(f"{original_score=:.2f}")
+
+    audio = audio[:, : reconstructed.size(-1)]
+
+    torchaudio.save(sample_dir / "audio.wav", audio, watermark.sample_rate)
+    torchaudio.save(sample_dir / "applied.wav", applied, watermark.sample_rate)
+    torchaudio.save(
+        sample_dir / "reconstructed.wav", reconstructed, watermark.sample_rate
+    )
+
+    applied_score = watermark.check(audio=applied)[0]
+    print(f"{applied_score=:.2f}")
+
+    shifted_audio = pad(applied, (123, 0))
+    shifted_score = watermark.check(audio=shifted_audio)[0]
+    print(f"{shifted_score=:.2f}")
+
+    run(
+        [
+            "ffmpeg",
+            "-hide_banner",
+            "-y",
+            "-i",
+            sample_dir / "applied.wav",
+            "-c:a",
+            "libopus",
+            "-b:a",
+            "16k",
+            sample_dir / "applied.ogg",
+        ],
+        check=True,
+        capture_output=True,
+    )
+
+    opus, sample_rate = torchaudio.load(sample_dir / "applied.ogg")
+    opus_score = watermark.check(audio=opus, sample_rate=sample_rate)[0]
+
+    print(f"{opus_score=:.2f}")
---
+++ watermark.py
@@ -0,0 +1,68 @@
+from torch import Tensor, float32, inference_mode, load, stack, tensor
+from torch.nn.functional import pad
+from torchaudio.functional import resample
+
+from key import Key
+from model import Model
+
+
+class Watermark:
+    @inference_mode()
+    def __init__(self, *, checkpoint: str, key: Key, sample_rate: int, shift: int):
+        state_dict = load(checkpoint, weights_only=True)
+        model = Model(channel_count=key.channel_count).cuda()
+        model.load_state_dict(state_dict, strict=True)
+        model.eval()
+        # model.encoder.compile(fullgraph=True, dynamic=True, mode="reduce-overhead")
+        # model.decoder.compile(fullgraph=True, dynamic=True, mode="reduce-overhead")
+
+        self.model = model
+        self.key = key
+        self.sample_rate = sample_rate
+        self.shift = shift
+
+    @inference_mode()
+    def check(
+        self,
+        *,
+        audio: Tensor,
+        sample_rate: int | None = None,
+        apply: bool = False,
+        reconstruct: bool = False,
+    ) -> tuple[float, None | Tensor, None | Tensor]:
+        audio = audio.cuda()
+
+        if sample_rate and sample_rate != self.sample_rate:
+            audio = resample(audio, sample_rate, self.sample_rate)
+
+        half_window = self.sample_rate // 100
+        batch = stack(
+            [
+                pad(audio, (x, half_window - x))
+                for x in range(0, half_window, self.shift)
+            ]
+        )
+
+        latents_batch = [
+            [[int(x) for x in xs] for xs in xss.T] for xss in self.model.encoder(batch)
+        ]
+        original_latents = tensor(latents_batch[0], dtype=float32, device="cuda").T
+
+        high_score = 0
+
+        for i, latents in enumerate(latents_batch):
+            score = self.key.check(latents, apply=(i == 0 and apply))
+            high_score = max(high_score, score)
+
+        applied = None
+
+        if apply:
+            applied_latents = tensor(latents_batch[0], dtype=float32, device="cuda").T
+            applied = self.model.decoder(applied_latents).cpu().squeeze(0)
+
+        reconstructed = None
+
+        if reconstruct:
+            reconstructed = self.model.decoder(original_latents).cpu().squeeze(0)
+
+        return high_score, applied, reconstructed
